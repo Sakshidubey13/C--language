b22302282489df34878f58756375bfbfe816bca9
Dubey Sakshi <sakshidubey1304@gmail.com>
2025-06-20 09:33:05 +0530

    Add files via upload

--- xy6.c
+++ xy6.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<conio.h>
   int main ()
-  {
+  { 
   	int x,y,z,result;
   	
   	printf("Enter value of X : ");//input value x
@@ -15,13 +15,15 @@
   	printf("Enter value of Z : ");//input value z
   	scanf("%d", &z);//user input the z variable
 
-  	
-  	//calculate of (x+y+z)3 
-  	result = (x*x*x) + (y*y*y) + (z*z*z) + (3*x + y*y + z*z + x);
-  	
-  	//output the result
-  	printf("Result of (x+y+z)3 = %d\n",result);
-  	
-  	return 0;
-  	
-  }
+   //calculate of (x-y-z)3 = x3-3x2(y+z)+3x(y+z)2-(y+z)3
+   result = (x*x*x) - (3*x*x*y+z) + (3*x*y*y+2*y*z+z*z) - (y*y*y+3*y*y*z+3*y*z*z+z*z*z);
+   
+   //output the result
+   printf("Result of (x-y-z)3 = %d\n", result);
+   
+   return 0;
+   
+}
+
+
+ 
