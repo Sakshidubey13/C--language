d82f6763bd42a5af15112a0cf41aee8db026406a
Dubey Sakshi <sakshidubey1304@gmail.com>
2025-06-22 20:35:04 +0530

    Add files via upload

--- vote1.c
+++ vote1.c
@@ -1,27 +1,42 @@
 #include<stdio.h>
 #include<conio.h>
 
-void main()
+ void main()
  {
-    int age;
-
-    printf("Enter your age: ");//input the age
-    scanf("%d", &age);//user enter the age
-
- 
-    if(age >= 18) {
-        printf("You are eligible for vote ! ");
-    }
-    else 
-	{
-        // Check if user is eligible to vote
-        if(age >= 18) 
-		{
-            printf("You are eligible to vote ! ");//print out
-        }
-		 else //(age<18)
-		{
-            printf("You are not eligible to vote !");//print out
-        }
-    }
-}
+ 	//Voting eligibility with Nested if & Validation .
+ 	int age;
+ 	
+ 	printf("Enter your age : ");//input enter your age
+ 	scanf("%d", &age);//user input the age
+ 	
+ 	if(age >= 18)
+ 	{
+ 		if(age > 100)
+ 		{
+ 			printf("You can not add age more than 100 !");//print output
+		 }
+		 else
+		 {
+		 	printf("You are eligible to vote !");//print output
+		 }
+	 }
+	 else
+	 {
+	 	if(age <= 0)
+	 	{
+	 		if(age == 0)
+	 		{
+	 		    printf("0 is not allowed !");//print output	
+			 }
+			 else
+			 {
+			 	printf("Negative age is not allowed !");//print output
+			 }
+		 }
+		 else
+		 {
+		 	printf("You are not eligible to vote !");//print output
+		 }
+	 }
+	 return 0;
+ }
