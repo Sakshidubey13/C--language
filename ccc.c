e68c5d858a8cebc785d0284fd321cd4f47d08133
Dubey Sakshi <sakshidubey1304@gmail.com>
2025-06-23 22:50:29 +0530

    Add files via upload

---
+++ ccc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main()
+{
+	int x = 20000;
+	int y = 90000;
+	int z;
+	
+	z = x;
+	x = y;
+    y = z;
+    
+    printf("%d\n", x);
+    printf("%d\n",y);
+}
