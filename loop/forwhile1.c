db64a62dcf22bb96251d4ea17c1f01fc5f81a11b
Dubey Sakshi <sakshidubey1304@gmail.com>
2025-06-27 16:58:49 +0530

    Add files via upload

---
+++ loop/forwhile1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    // wap to print 1 to 10 using for_while loop.
+
+    int i;
+
+    for (i = 1; i <= 10; i++) //start condion, I less than equal to 10., end condition
+    {
+        printf("%d\n",i);//print
+    }
+}
---
+++ loop/forwhile2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    // wap to 10 to 1 using for_while loop.
+    int i = 10;
+
+    for ( i = 10; i >=1; i--)//start condition , I GREATER THAN 1 and end condition:
+    {
+      printf("%d\n",i);//print out
+    }
+    
+}
