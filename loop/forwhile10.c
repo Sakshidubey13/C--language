4f78a67af13a76abf2aa46b876464c1d7f66d013
Dubey Sakshi <sakshidubey1304@gmail.com>
2025-06-29 23:19:25 +0530

    Add files via upload

---
+++ loop/forwhile10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+	int start;
+	
+	for (start = 2000; start <= 5000; start += 4)//Initialization: Loop starts with start = 2000. (First value is 2000.)
+           //Condition: Loop will continue as long as start is less than or equal to 3000. It will stop when start becomes more than 3000.
+                    //start += 4	Increment: After each loop round, increase start by 4. (Same as: start = start + 4)
+
+
+	
+	
+	
+	
+	
+	{
+		printf("%d\t",start);
+	}
+}
