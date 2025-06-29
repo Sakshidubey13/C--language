c38dde493f144e438957523f7352387af69e98ce
Dubey Sakshi <sakshidubey1304@gmail.com>
2025-06-29 22:41:56 +0530

    Add files via upload

--- loop/forwhile7.c
+++ loop/forwhile7.c
@@ -1,14 +1,23 @@
-#include<stdio.h>
+#include <stdio.h> 
 
 void main()
 {
-	//wap to print leap year using for_while loop.
-	
-	int start;
+    // WAP to print sum 1 to N using for_loop.
 
-	
-	for(start = 2000; start <= 3000; start +=4)//start condition, start less than 3000(condition T), start = 2000+4 = 2004 )(increment condition).
-	{
-		printf("%d\t",start);//print out
-	}
+    int n;
+    int i = 1, sum = 0;
+
+    printf("Enter N:"); // Input message for N
+    scanf("%d", &n);    // User input to N
+
+for (i = 1;i <= n; i++)//Start: We are starting with i = 1. First value of i is 1.
+                     //Condition: Keep running the loop as long as i is less than or equal to N. This means loop will stop after reaching N.
+                    //ncrement: After every loop round, increase the value of i
+
+{
+	sum = sum + i;
+}
+   printf("Sum is = %d",sum);
+   
+   return 0;
 }
