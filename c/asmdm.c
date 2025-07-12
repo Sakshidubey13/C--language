414a3d7a6831a2acda2183b4da871b4975476354
Dubey Sakshi <sakshidubey1304@gmail.com>
2025-07-12 23:49:02 +0530

    Add files via upload

---
+++ c/asmdm.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+{
+    int a = 30;
+    int b = 10;
+    
+    //addition
+    printf("Addition = %d\n\n", a+b);
+
+    //subtraction
+printf("subtration =  %d\n\n", a-b);
+
+     //multipition
+     printf("Multiply = %d\n\n", a*b);
+
+     //divion
+     printf("Division = %d\n\n", a/b);
+
+     //modulus = quotient
+     printf("modulus = %d\n", a%b);
+
+}
