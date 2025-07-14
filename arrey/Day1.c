981f63037e358d6de6c11757c6d402624cba2283
Dubey Sakshi <sakshidubey1304@gmail.com>
2025-07-14 15:59:38 +0530

    Add files via upload

---
+++ arrey/Day1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+
+    int i;
+    int a[10] = {91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
+
+    for (i = 0; i <= 9; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
