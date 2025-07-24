61d1514dc157afac132c0e770bd7beac316f5b90
Dubey Sakshi <sakshidubey1304@gmail.com>
2025-07-24 23:14:06 +0530

    Update Q4.c

--- Function/Task-1 Leb work/Q4.c
+++ Function/Task-1 Leb work/Q4.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <conio.h>
 
+// wap to print 1 to n using function.ex.. 1 2 3 4 5 .......
+
+
 void num(int n)
 {
     int i;
     for (i = 1; i <= n; i++)
     {
-        printf("%d", &i);
+        printf("%d ", i);
     }
 }
 void main()
 {
     int n;
 
-    printf("Enter Number : ");
-    scanf("%d", &n);
+    printf("Enter Number : ");//user input any number 
+    scanf("%d ", &n);//user n
 
-    num(n);
+    num(n);//as go for return void sum
+    
 }
